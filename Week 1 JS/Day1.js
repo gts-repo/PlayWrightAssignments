78fdd13513a17016b41fdf65d84f07d5119df0de
gts-repo <94696446+gts-repo@users.noreply.github.com>
2025-10-15 23:56:55 +0530

    Add files via upload
    Week 1

---
+++ Week 1 JS/Day1.js
@@ -0,0 +1,11 @@
+ var firstName = "FName";
+ var companyName = "Company Name";
+ var mobileNumber = 9090909090;
+ var isAutomation = true;
+ var hasPlayWright;
+
+  console.log("Type of firstName -> " + typeof firstName);
+  console.log("Type of companyName -> " + typeof companyName);
+  console.log("Type of mobileNumber -> " + typeof mobileNumber);
+  console.log("Type of isAutomation -> " + typeof isAutomation);
+  console.log("Type of hasPlayWright -> " + typeof hasPlayWright);
